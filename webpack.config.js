a3703b4ec0daca54da078fb6f84d488d36654921
Vinu <vvinayan538@gmail.com>
2022-01-22 18:59:17 +0530

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
                     loader: "file-loader",
                     options: {
                         name: "[name].[hash].[ext]",
-                        outputPath: "imgs"
+                        outputPath: "images"
                     }
                 }
             }
